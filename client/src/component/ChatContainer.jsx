32a8e0f3b45d5c80b8a2b2c492e2552a702a681e
Anshuman-sharma2006 <anshumansharma9451@gmail.com>
2025-11-09 00:47:05 +0530

    ChatContainer template
    chat container is on way working progress

--- client/src/component/ChatContainer.jsx
+++ client/src/component/ChatContainer.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import assets, { messagesDummyData } from '../assets/assets'
 
-export const ChatContainer = () => {
-  return (
-    <div>ChatContainer</div>
+ const ChatContainer = ({ selectedUser, setSelectedUser })=> 
+  {
+  return selectedUser ? 
+  (
+    <div className='h-full overflow-scroll relative backdrop-blur-lg'>
+      <div className='flex item-center gap-3 py-3 mx-4 border-b border-stone-500'>
+        <img src={assets.profile_martin} alt="" srcset=""  className=' w-8 rounded-full'/>
+        <p className=' flex-1 text-lg text-red flex item-center gap-2'>martin jhonson</p>
+        <span className='w-2 h-2 rounded-full bg-green-500'></span>
+        <img  onClick={() => setSelectedUser(null)} src={assets.arrow_icon} alt="" srcset=""className='md:hidden max-w-7' />
+       <img src={assets.help_icon} alt="" srcset="" className='max-md:hidden max-w-5' />
+      <div className="flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3">
+{messagesDummyData.map((msg,index) => (
+  <div key={index} className={`flex items-end gap-2 justify-end gap-2 ${msg.sender !== "680f50e4f10f3cd28382ecf9"&& 'flex-row-reverse'}`}>
+(msg.image ?(
+  <img src={assets.image} alt="" srcset="" className='w-10 h-10 object-cover rounded-full' />
+):(
+  <p className={``}>{msg.text}</p>
+))
+  </div>
+  
+))}
+      </div>
+      </div>
+      </div>
+     
+  ) : 
+  (
+    <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
+      <img src={assets.logo_icon} alt="" className='max-w-16' />
+      <p className='text-large font-medium text-white' > Chat,Anytime and Anywhere</p>
+    </div>
   )
-}
+
+       }
+
 export default ChatContainer
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{ useState }  from 'react'
 /* Rembere That when ur rendering and bringing something check import file first
 ./=>current folder(same folder operation )
 ../=>parent folder(go open up more like order folder to perform when your on another folder,another folder perform on another folder)
@@ -7,20 +7,22 @@ import SideBar from '../component/SideBar'
 import ChatContainer from '../component/ChatContainer'  
 import RightSideBar from '../component/rightSideBar'
 export const Homepage = () => {
+  const [selecteduser, setSelectedUser] = useState(false);
   return (
     // px=>x axis padding on left/right;h=>height
     // py=>y axis padding on top/bottom;w=>width
-
-    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%] '>
-        <div className='backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative'>
-          <SideBar/>
-          <ChatContainer/>
-          <RightSideBar/>
+    <div className=' border w-full h-screen sm:px-[15%] sm:py-[5%] '>
+        <div 
+        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selecteduser ? "md:grid-cols-3" : "md:grid-cols-2"}`}>
+          {/* selectedUser={selecteduser} setSelectedUser={setSelectedUser} */}
+          <SideBar selectedUser={selecteduser} setSelectedUser={setSelectedUser}  />
+          <ChatContainer selectedUser={selecteduser} setSelectedUser={setSelectedUser}/>
+          <RightSideBar selectedUser={selecteduser} setSelectedUser={setSelectedUser}/>
 
          
         </div>
         
     </div>
-  )
-}
+  );
+};
 export default Homepage
